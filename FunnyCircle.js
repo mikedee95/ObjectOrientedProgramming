6a686841673e7a42ebd44afaf05cbbcfe3084045
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2020-09-23 16:31:00 +0700

    Add files via upload

---
+++ FunnyCircle.js
@@ -0,0 +1,24 @@
+function Circle(x,y,radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+// function creatCircle(){
+//     let ctx = document.getElementById("myCanvas").getContext("2d");
+//     let circle = new Circle(500,500,80);
+//     ctx.beginPath();
+//     ctx.arc(circle.x, circle.y, circle.radius, 0 , 2* Math.PI);
+//     ctx.fill();
+// }
+// creatCircle();
+
+// function  creatCircle2(){
+//     let ctx = document.getElementById("myCanvas").getContext("2d");
+//     let radius = Math.floor(Math.random()*80);
+//     let circle2 = new Circle(500,500, radius);
+//     ctx.beginPath();
+//     ctx.arc(circle2.x, circle2.y, circle2.radius, 0, 2*Math.PI);
+//     ctx.fill();
+// }
+// creatCircle2()
+
