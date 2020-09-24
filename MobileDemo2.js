c83e99e09f4697dafba4443d41c6961c0e1dd585
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2020-09-24 16:31:58 +0700

    Update MobileDemo2.js

--- MobileDemo2.js
+++ MobileDemo2.js
@@ -5,30 +5,34 @@ function Mobile(battery, draft, inbox, sent, status) {
     this.sent = sent;
     this.status = status;
 
-    this.isOn = function () {
-        return this.status
-    };
-    this.turnOn = function () {
-        this.status = true
-    };
-    this.turnOff = function () {
-        this.status = false
-    };
+    // this.isOn = function () {
+    //     return this.status
+    // };
+    // this.turnOn = function () {
+    //     this.status = true
+    // };
+    // this.turnOff = function () {
+    //     this.status = false
+    // };
     this.charge = function () {
         if (this.battery < 100) {
             this.battery++
         }
     };
     this.textingMessage = function (message) {
-        this.draft = message
+        this.draft = message;
+        this.batteryUsage()
     };
     this.receiveMessage = function (message) {
-        this.inbox.push(message)
+        this.inbox.push(message);
+        this.batteryUsage();
     };
     this.sendMessage = function (mobile) {
         mobile.receiveMessage(this.draft)
+        this.batteryUsage()
     };
     this.getInbox = function () {
+        this.batteryUsage()
         return this.inbox
     };
     this.batteryUsage = function (){
@@ -44,4 +48,5 @@ nokia.getInbox();
 nokia.textingMessage("Hello");
 nokia.sendMessage(iphone);
 iphone.getInbox();
-document.write("Iphone says : " + nokia.getInbox() + " and Nokia says: " + iphone.getInbox());
+// document.write("Iphone says : " + nokia.getInbox() + " and Nokia says: " + iphone.getInbox());
+document.write("The currently battery is: " +  nokia.battery)
