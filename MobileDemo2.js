dd98415d4d2c42d43906fa49a724f5cb91f8483e
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2020-09-24 16:26:04 +0700

    Add files via upload

---
+++ MobileDemo2.js
@@ -0,0 +1,47 @@
+function Mobile(battery, draft, inbox, sent, status) {
+    this.battery = battery;
+    this.draft = draft;
+    this.inbox = inbox;
+    this.sent = sent;
+    this.status = status;
+
+    this.isOn = function () {
+        return this.status
+    };
+    this.turnOn = function () {
+        this.status = true
+    };
+    this.turnOff = function () {
+        this.status = false
+    };
+    this.charge = function () {
+        if (this.battery < 100) {
+            this.battery++
+        }
+    };
+    this.textingMessage = function (message) {
+        this.draft = message
+    };
+    this.receiveMessage = function (message) {
+        this.inbox.push(message)
+    };
+    this.sendMessage = function (mobile) {
+        mobile.receiveMessage(this.draft)
+    };
+    this.getInbox = function () {
+        return this.inbox
+    };
+    this.batteryUsage = function (){
+        this.battery --
+    }
+}
+
+let iphone = new Mobile(100, "", [], [], true);
+let nokia = new Mobile(100, "", [], [], true);
+iphone.textingMessage("Hi there");
+iphone.sendMessage(nokia);
+nokia.getInbox();
+nokia.textingMessage("Hello");
+nokia.sendMessage(iphone);
+iphone.getInbox();
+document.write("Iphone says : " + nokia.getInbox() + " and Nokia says: " + iphone.getInbox());
